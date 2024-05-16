2b16f855a50b21be870ec0e561ccb197484919d2
tamongbo <tamongbo@gmail.com>
2024-05-16 16:39:59 +0900

    [feature] 33

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,5 +1,12 @@
 #include "pch.h"
+#include "../test/prime-factors.cpp"
 
-TEST(PrimeFactors, PrimeTest) {
-  EXPECT_EQ(1, 1);
+#include <vector>
+using namespace std;
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
--- test/prime-factors.cpp
+++ test/prime-factors.cpp
@@ -1,3 +1,10 @@
-class PrimeFactor {
+#include <vector>
 
+using namespace std;
+
+class PrimeFactor {
+public:
+	vector<int> of(int i) {
+		return {};
+	}
 };
